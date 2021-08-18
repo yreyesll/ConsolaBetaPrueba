76ceed7b4c9d4f5a1006d6afcba1c5ae1aecc335
vladycruz <vcruz.segovia@gmail.com>
2021-08-18 12:20:32 -0500

    otro cambio mas

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApp1Beta
            
             Console.WriteLine("esto es un cambio a las 11:55 am");
             Console.Write("esto es un cambio a las 11:59 am");
+            Console.Write("esto es un cambio a las 12:20pm");
             Console.ReadKey();
         }
     }
