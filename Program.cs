7d819be66eb05ed6a110d8cdb838c67064228c97
vladycruz <vcruz.segovia@gmail.com>
2021-08-18 11:51:12 -0500

    primer commit y subida oficial

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace ConsoleApp1Beta
             
             Console.WriteLine("Hola padrino quiero a juanita cacharla y riko de una vez3");
             Console.WriteLine("Hola padrino quiero a juanita cacharla y riko de una vez2");
+            Console.WriteLine("Esto es un nuevo cambio carajo ");
+            Console.WriteLine("por favor activate");
+            Console.WriteLine("quiero cachar , follar , fornicar, chifar");
+            Console.Write("esto es un cambio a las 11:46 am");
         }
     }
 }
