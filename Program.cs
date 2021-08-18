18ab761cdb123c4eb6cfe638e907a462fe8653d6
vladycruz <vcruz.segovia@gmail.com>
2021-08-18 11:56:30 -0500

    segunda subida de commiy y prueba

--- Program.cs
+++ Program.cs
@@ -7,12 +7,9 @@ namespace ConsoleApp1Beta
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Hola padrino quiero a juanita cacharla y riko de una vez3");
-            Console.WriteLine("Hola padrino quiero a juanita cacharla y riko de una vez2");
-            Console.WriteLine("Esto es un nuevo cambio carajo ");
-            Console.WriteLine("por favor activate");
-            Console.WriteLine("quiero cachar , follar , fornicar, chifar");
-            Console.Write("esto es un cambio a las 11:46 am");
+           
+            Console.Write("esto es un cambio a las 11:55 am");
+            Console.Write("esto es un cambio a las 11:56 am");
         }
     }
 }
