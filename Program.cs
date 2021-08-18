e96960c9b28a6744e75bb4e3e5bc302b0696795c
vladycruz <vcruz.segovia@gmail.com>
2021-08-18 11:21:29 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1Beta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hola padrino quiero a juanita cacharla y riko de una vez3");
+            Console.WriteLine("Hola padrino quiero a juanita cacharla y riko de una vez2");
+        }
+    }
+}
