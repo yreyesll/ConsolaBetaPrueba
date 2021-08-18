085fb7bb9ac4e0c7bf5a825f75e71083fbb76cc0
vladycruz <vcruz.segovia@gmail.com>
2021-08-18 12:17:48 -0500

    creacion de carpetas online

---
+++ Dto/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1Beta.Dto
+{
+    class Cliente
+    {
+        public int id { get; set; }
+        public string nombres { get; set; }
+        public string apellidos { get; set; }
+        public string Dni { get; set; }
+        public string correo { get; set; }
+    }
+}
