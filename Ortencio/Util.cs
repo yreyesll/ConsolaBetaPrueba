e5f2752324b49df510d3fe73d79ad851fedecf75
Vladimir Cruz <vcruz.segovia@gmail.com>
2021-08-18 12:45:48 -0500

    Add files via upload

---
+++ Ortencio/Util.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace ConsultaRuc
+{
+    public class Util
+    {
+        public static string LeerTextoDeImagen(byte[] imagenBytes)
+        {
+            //string DataPath = "";
+            string TextoResultado = null;
+            float porcentaje = 0;
+            int numIntentos = 1;
+            try
+            {
+                while (porcentaje <= 0.50 && numIntentos <= 3)
+                {
+                    using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+                    {
+                        using (var img = Pix.LoadFromMemory(imagenBytes))
+                        {
+
+                            using (var page = engine.Process(img))
+                            {
+
+                                TextoResultado = page.GetText().Trim();
+                                porcentaje = page.GetMeanConfidence();
+                                numIntentos++;
+
+                            }
+                        }
+
+                    }
+                    if (porcentaje >= 0.50)
+                    {
+                        break;
+                    }
+                }
+                return TextoResultado;
+
+            }
+            catch (Exception ex)
+            {
+                return null;
+
+            }
+
+        }
+
+    }
+}
