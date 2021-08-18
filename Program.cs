07962b1fce5684af78264ff603c9a7f0ec56b46e
vladycruz <vcruz.segovia@gmail.com>
2021-08-18 12:04:01 -0500

    tercer commit creo para la prueba de git con visual studio 2019

--- Program.cs
+++ Program.cs
@@ -8,8 +8,9 @@ namespace ConsoleApp1Beta
         {
             
            
-            Console.Write("esto es un cambio a las 11:55 am");
-            Console.Write("esto es un cambio a las 11:56 am");
+            Console.WriteLine("esto es un cambio a las 11:55 am");
+            Console.Write("esto es un cambio a las 11:59 am");
+            Console.ReadKey();
         }
     }
 }
